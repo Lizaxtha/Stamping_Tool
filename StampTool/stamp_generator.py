4bb6cad9fdd3ceed47265c051de64404e15fb9a3
Liza Shrestha <shresthaliza4@gmail.com>
2026-08-30 18:26:26 +0545

    connected to tools/script, created dialogue box

--- StampTool/stamp_generator.py
+++ StampTool/stamp_generator.py
@@ -1,3 +1,58 @@
-from krita import krita
+from krita import *
+from PyQt5.QtWidgets import (
+    QDialog,
+    QVBoxLayout,
+    QLabel,
+    QPushButton,
+    QListWidget,
+    QFileDIalog
+)
 
-print("Stamp Tool loaded!")
+class StampDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Stamps")
+        self.setMinimumWidth(400)
+
+        self.stamps=[]
+
+        layout =QVBoxLayout()
+
+        # title = QLabel("Stamp")
+        # layout.addWidget(title)
+
+        description = QLabel(
+            "Choose one or more stamps"
+        )
+        layout.addWidget(description)
+
+        custom_button = QPushButton("Custom Stamp")
+        layout.addWidget(custom_button)
+
+        self.setLayout(layout)
+class StampTool(Extension):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def setup(self):
+        pass
+
+    def createActions(self,window):
+        action=window.createAction(
+            "stamp_tool_test",
+            "Stamp Tool test",
+            "tools/scripts"
+        )
+
+        action.triggered.connect(self.show_dialog)
+
+    def show_dialog(self):
+        dialog = StampDialog()
+        dialog.exec_()
+
+
+Krita.instance().addExtension(
+    StampTool(Krita.instance())
+)
---
+++ journal.md
@@ -0,0 +1,2 @@
+rough idea sketch
+![alt text](IMG_20260830_180824.jpg)
